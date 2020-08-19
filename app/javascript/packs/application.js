4bc89c114129f6a7519686bef90286dca9d96a7c
BranLiang <lby89757@hotmail.com>
2020-08-19 14:13:33 +0800

    Add the tailwind css framework

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@
 
 require("@rails/ujs").start()
 
+// Tailwind CSS
+import "stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
