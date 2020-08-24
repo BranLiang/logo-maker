ecb98548cc167507bc6541c632fcb56cd478e8d5
BranLiang <lby89757@hotmail.com>
2020-08-24 13:09:58 +0800

    Add vertical slider

--- app/javascript/controllers/logo_controller.js
+++ app/javascript/controllers/logo_controller.js
@@ -3,6 +3,10 @@ import domtoimage from 'dom-to-image';
 
 export default class extends Controller {
   connect() {
+    const element = document.getElementById("logo")
+    const attributes = getComputedStyle(element)
+    this.paddingTop = parseInt(attributes.paddingTop)
+    this.paddingBottom = parseInt(attributes.paddingBottom)
     document.addEventListener('font-switch', ({ detail }) => {
       const logo = document.getElementById("logo")
       logo.style.fontFamily = detail.family
@@ -23,5 +27,13 @@ export default class extends Controller {
   round(e) {
     const element = document.getElementById("logo")
     element.style.borderRadius = `${e.target.value}px`
+  }
+
+  verticalMove(e) {
+    const element = document.getElementById("logo")
+    const attributes = getComputedStyle(element)
+    const move = e.target.valueAsNumber
+    element.style.paddingTop = `${this.paddingTop - move}px`
+    element.style.paddingBottom = `${this.paddingBottom + move}px`
   }
 }
